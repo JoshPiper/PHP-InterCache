38ba9c392ccf8af28057eb0614b53660b05d85ea
John Internet <jonjon1234.github@gmail.com>
2019-08-11 13:59:42 +0100

    Add cache result. interface.

---
+++ src/CacheResult.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Internet\InterCache;
+
+/**
+ * The cache interface allows overwriting of individual implementations of cache results.
+ * Interface CacheResult
+ * @package Internet\InterCache
+ */
+interface CacheResult {
+	/** Check if this result has expired.
+	 * @return bool
+	 */
+	public function expired() : bool;
+
+	/** Fetch the value of this result.
+	 * @return bool
+	 */
+	public function value() : bool;
+};
