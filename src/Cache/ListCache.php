0684326974509a46061eb54a4ec0ee1f7bf24d7f
John Internet <jonjon1234.github@gmail.com>
2019-08-14 19:04:58 +0100

    Convert ListCache to PSR-6 compliance.

--- src/Cache/ListCache.php
+++ src/Cache/ListCache.php
@@ -2,41 +2,84 @@
 
 namespace Internet\InterCache\Cache;
 use DateTimeInterface;
-use Internet\InterCache\Exceptions\CacheStoreException;
-use Internet\InterCache\Result\CacheResult;
+use Internet\InterCache\Exceptions\InvalidKeyException;
 use Internet\InterCache\Result\StdCacheResult;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
 
-abstract class ListCache implements Cache {
+/**
+ * Class ListCache
+ * @package Internet\InterCache\Cache
+ */
+abstract class ListCache implements CacheItemPoolInterface {
 	protected $data = [];
 
-	public function has(string $key): bool{
+	protected static function illegalKey($key): bool{
+		return !preg_match("/[^\w_\-.]/", $key);
+	}
+
+	/**
+	 * @param string $key
+	 * @return StdCacheResult
+	 * @throws InvalidKeyException
+	 */
+	public function getItem($key): StdCacheResult{
+		if ($this::illegalKey($key)){throw new InvalidKeyException();}
+
+		if (isset($this->data[$key])){
+			[$expiry, $data] = $this->data[$key];
+			return new StdCacheResult($key, $data, $expiry);
+		} else {
+			return new StdCacheResult($key);
+		}
+	}
+
+	/**
+	 * @param array $keys
+	 * @return array
+	 * @throws InvalidKeyException
+	 */
+	public function getItems(array $keys = []): array{
+		if (count($keys) === 0){
+			return [];
+		} elseif (count(array_filter($keys, [$this, 'illegalKey'])) > 0){
+			throw new InvalidKeyException();
+		} else {
+			return array_map([$this, 'getItem'], $keys);
+		}
+	}
+
+	public function hasItem($key){
+		if ($this::illegalKey($key)){throw new InvalidKeyException();}
 		return isset($this->data[$key]);
 	}
 
-	public function pull($key): ?CacheResult{
-		if (!$this->has($key)){return null;}
-
-		[$expiry, $data] = $this->data[$key];
-		return new StdCacheResult($data, $expiry);
+	public function clear(){
+		$this->data = [];
+		$this->commit();
 	}
 
-	/** Store a value in the cache.
-	 * @param string $key
-	 * @param mixed $value
-	 * @param int|DateTimeInterface $ttl
-	 * @throws CacheStoreException
-	 */
-	public function store(string $key, $value, $ttl = 60): void{
-		$expiry = false;
-		if (is_numeric($ttl)){
-			$expiry = time() + floatval($ttl);
-		} elseif ($ttl instanceof DateTimeInterface){
-			$expiry = $ttl->getTimestamp();
+	public function deleteItem($key){
+		if ($this::illegalKey($key)){throw new InvalidKeyException();}
+		$this->commit();
+		return true;
+	}
+	public function deleteItems(array $keys){
+		if (count($keys) === 0){
+			return [];
+		} elseif (count(array_filter($keys, [$this, 'illegalKey'])) > 0){
+			throw new InvalidKeyException();
+		} else {
+			array_map([$this, 'deleteItem'], $keys);
+			return true;
 		}
-		if (!$expiry){
-			throw new CacheStoreException("Failed to generate expiry from ttl.");
-		}
+	}
 
-		$this->data[$key] = [$expiry, $value];
+	public function save(CacheItemInterface $item){
+		$this->saveDeferred($item);
+		$this->commit();
+	}
+	public function saveDeferred(CacheItemInterface $item){
+		$this->data[$item->getKey()] = [$item->getExpiry(), $item->get()];
 	}
 }
